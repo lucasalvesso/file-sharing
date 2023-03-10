bed1464e42593a5c843e5d93ffbec272c44873cd
Lucas Oliveira <lucas.oliveira@grupogera.com>
2023-03-09 23:01:03 -0300

    deleted dist

--- app/dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-import { HandlerRouteResponse } from "./infrastcructure/interface/HandlerRouteResponse";
-import { Request } from "express";
-export declare const handler: (event: Request) => Promise<HandlerRouteResponse>;
