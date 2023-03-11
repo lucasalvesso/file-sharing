9c89881923fd169a1b19ef3317c2b984554efb98
Lucas Oliveira <lucas.oliveira@grupogera.com>
2023-03-11 16:18:56 -0300

    adjust to workflow

--- frontend/scripts/gh-pages-deploy.js
+++ frontend/scripts/gh-pages-deploy.js
@@ -6,10 +6,10 @@ import * as fs from "fs";
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
 
     console.log("Building started...");
-    await execa("npm", ["run", "build", "--prefix", "frontend"]);
+    await execa("npm", ["run", "build"]);
 
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("./frontend/dist") ? "dist" : "build";
+    const folderName = fs.existsSync("./dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
